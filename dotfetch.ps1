2b8e6b1e8cda034d595f15f35550f0547daa5d4d
Ibne Nahian <evilprince2009@gmail.com>
2021-09-19 16:22:26 +0600

    conf

--- dotfetch.ps1
+++ dotfetch.ps1
@@ -57,7 +57,7 @@ $is_pscore = if ($PSVersionTable.PSEdition.ToString() -eq 'Core') {
 $configdir = $env:XDG_CONFIG_HOME, "${env:USERPROFILE}\.config" | Select-Object -First 1
 $config = "${configdir}/dotfetch/config.ps1"
 
-$defaultconfig = ''
+$defaultconfig = 'https://github.com/evilprince2009/DotFetch/blob/main/lib/config.ps1'
 
 # ensure configuration directory exists
 if (-not (Test-Path -Path $config)) {
