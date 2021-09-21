f4a3f42b6b5cb85c54b161f46dd939fd8d44dff2
Ibne Nahian <evilprince2009@gmail.com>
2021-09-21 09:58:10 +0600

    image removed

--- dotfetch.ps1
+++ dotfetch.ps1
@@ -86,7 +86,6 @@ if ($genconf.IsPresent) {
     exit 0
 }
 
-
 # ===== VARIABLES =====
 $disabled = 'disabled'
 $strings = @{
@@ -111,7 +110,6 @@ $strings = @{
     battery     = ''
     kernel      = ''
 }
-
 
 # ===== CONFIGURATION =====
 [Flags()]
@@ -186,54 +184,6 @@ $img = if (-not $image -and -not $noimage.IsPresent -and $strings.os -Match 'Win
             "                                        ",
             "                                        ";
     )
-}
-
-elseif (-not $noimage.IsPresent -and $image) {
-    if (-not (Get-Command -Name magick -ErrorAction Ignore)) {
-        Write-Output 'error: Imagemagick must be installed to print custom images.' -f red
-        Write-Output 'hint: if you have Scoop installed, try `scoop install imagemagick`.' -f yellow
-        exit 1
-    }
-
-    $COLUMNS = 35
-    $CURR_ROW = ""
-    $CHAR = [Text.Encoding]::UTF8.GetString(@(226, 150, 128)) # 226,150,136
-    $upper, $lower = @(), @()
-
-    if ($image -eq 'wallpaper') {
-        $image = (Get-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name Wallpaper).Wallpaper
-    }
-    if (-not (test-path -path $image)) {
-        Write-Output 'Specified image or wallpaper does not exist.' -f red
-        exit 1
-    }
-    $pixels = @((magick convert -thumbnail "${COLUMNS}x" -define txt:compliance=SVG $image txt:-).Split("`n"))
-    foreach ($pixel in $pixels) {
-        $coord = [regex]::Match($pixel, "([0-9])+,([0-9])+:").Value.TrimEnd(":") -split ','
-        $col, $row = $coord[0, 1]
-
-        $rgba = [regex]::Match($pixel, "\(([0-9])+,([0-9])+,([0-9])+,([0-9])+\)").Value.TrimStart("(").TrimEnd(")").Split(",")
-        $r, $g, $b = $rgba[0, 1, 2]
-
-        if (($row % 2) -eq 0) {
-            $upper += "${r};${g};${b}"
-        } else {
-            $lower += "${r};${g};${b}"
-        }
-
-        if (($row % 2) -eq 1 -and $col -eq ($COLUMNS - 1)) {
-            $i = 0
-            while ($i -lt $COLUMNS) {
-                $CURR_ROW += "${e}[38;2;$($upper[$i]);48;2;$($lower[$i])m${CHAR}"
-                $i++
-            }
-            "${CURR_ROW}${e}[0m"
-
-            $CURR_ROW = ""
-            $upper = @()
-            $lower = @()
-        }
-    }
 }
 else {
     @()
@@ -323,7 +273,6 @@ $strings.gpu = if ($configuration.HasFlag([Configuration]::Show_GPU)) {
 } else {
     $disabled
 }
-
 
 # ===== MEMORY =====
 $strings.memory = if ($configuration.HasFlag([Configuration]::Show_Memory)) {
